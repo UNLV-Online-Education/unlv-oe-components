d53f1afc455bc611038d540b676c9a58920683b5
John Murphy <dearjohnm@gmail.com>
2020-07-24 09:42:01 -0700

    updated stencil

--- src/components.d.ts
+++ src/components.d.ts
@@ -5,7 +5,7 @@
  * It contains typing information for all components that exist in this project.
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
-import { Flashcard, StyleOptions, } from "./components/flashcards/types";
+import { Flashcard, StyleOptions } from "./components/flashcards/types";
 export namespace Components {
     interface UnlvOeFeedbackButton {
         "buttonCss": object;
